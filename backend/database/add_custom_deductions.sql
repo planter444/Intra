1cb261802e59626c2f8393b3ee81ba9b5dfa5df2
planter444 <imhuabi@gmail.com>
2026-08-26 20:01:27 +0300

    Tarumbeta

--- backend/database/add_custom_deductions.sql
+++
@@ -1,2 +0,0 @@
--- Add custom_deductions column to payroll_profiles table
-ALTER TABLE payroll_profiles ADD COLUMN IF NOT EXISTS custom_deductions JSONB NOT NULL DEFAULT '[]'::jsonb;
--- backend/database/add_dsa_provided.sql
+++
@@ -1,3 +0,0 @@
--- Add dsa_provided column to travel_requests table
-ALTER TABLE travel_requests
-ADD COLUMN IF NOT EXISTS dsa_provided BOOLEAN DEFAULT FALSE;
